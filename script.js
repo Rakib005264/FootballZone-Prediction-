b0fdd23f249c28084a2da21751cc2a765d75d0b6
Rakib005264 <rakibahmedalamin2@gmail.com>
2026-07-19 14:53:49 +0600

    Add live football fixtures API fetch functionality

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const API_KEY = "তোমার_API_KEY";
 
+fetch("https://v3.football.api-sports.io/fixtures?live=all", {
+  method: "GET",
+  headers: {
+    "x-apisports-key": API_KEY
+  }
+})
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+})
+.catch(error => console.error(error));
